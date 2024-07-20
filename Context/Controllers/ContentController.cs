b09e2b687b7c7f4a6dd39dbb422076aa88fe9f1f
ickqkicx <ickqkicx@gmail.com>
2024-07-20 12:19:02 +0400

    update CotentController

--- Context/Controllers/ContentController.cs
+++ Context/Controllers/ContentController.cs
@@ -1,6 +1,8 @@
-﻿using Auth.Data;
+﻿using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using Auth.Data;
 
 namespace Auth.Controllers
 {
@@ -9,6 +11,49 @@ namespace Auth.Controllers
     public class ContentController(AuthDbContext dbContext) : ControllerBase
     {
         private readonly AuthDbContext _dbContext = dbContext;
+
+        [Authorize]
+        [HttpGet("AddClaimsSurname")]
+        public async Task<ActionResult> AddClaimsSurname()
+        {
+            var surname = string.Empty;
+            if (User.Identity is ClaimsIdentity claimsIdentity)
+            {
+                var name = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+                if (name == null) return BadRequest();
+
+                surname = new string(name.Reverse().ToArray());
+
+                claimsIdentity.AddClaim(new Claim(ClaimTypes.Surname, surname));
+                var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+
+                await HttpContext.SignInAsync(claimsPrincipal);
+            }
+
+            surname = User.FindFirst(ClaimTypes.Surname)?.Value;
+            if (surname == null) return BadRequest();
+
+            return Ok($"AddClaimsSurname: {surname}");
+        }
+
+        [Authorize]
+        [HttpGet("RemoveClaimsSurname")]
+        public async Task<ActionResult> RemoveClaimsSurname()
+        {
+            if (User.Identity is ClaimsIdentity claimsIdentity)
+            {
+                var surname = User.FindFirst(ClaimTypes.Surname);
+                //if (surname == null) return BadRequest();
+
+                if (claimsIdentity.TryRemoveClaim(surname))
+                {
+                    var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+                    await HttpContext.SignInAsync(claimsPrincipal);
+                }
+            }
+
+            return Ok($"RemoveClaimsSurname");
+        }
 
         [AllowAnonymous]
         [HttpGet("AllowAnonymous")]
@@ -22,8 +67,9 @@ namespace Auth.Controllers
         [HttpGet("Authorize")]
         public ActionResult Authorize()
         {
-            
-            return Ok("Authorize");
+            var name = User.FindFirst(ClaimTypes.NameIdentifier);
+
+            return Ok($"Authorize: {name}");
         }
 
         [Authorize(Roles = "admin")]
